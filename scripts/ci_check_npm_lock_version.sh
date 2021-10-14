dc1d6b10df9ba0341eb6008d6ddcfcfb8f8245ec
LensPlatform <yoanyombapro@gmail.com>
2021-10-13 20:03:24 -0400

    feat: update yarn lock file
    Signed-off-by: LensPlatform <yoanyombapro@gmail.com>

--- scripts/ci_check_npm_lock_version.sh
+++ scripts/ci_check_npm_lock_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REQUIRED_VERSION=1
-LOCKFILE_VERSION=$(sed -n 's/[ \t]*"lockfileVersion"\: \([0-9]*\),/\1/p' "$1")
+LOCKFILE_VERSION=$(sed -n 's/[ \t]*"lockfile"\: \([0-9]*\),/\1/p' "$1")
 
 if [[ $LOCKFILE_VERSION != "$REQUIRED_VERSION" ]]; then
     echo >&2 "Incorrect npm lock file version: $LOCKFILE_VERSION (Version $REQUIRED_VERSION required)"
